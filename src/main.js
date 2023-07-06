8f757531e3c6ffe9165767ff0c840ba8af27021b
Ajit Kumar <me@ajitkumar.dev>
2023-07-06 13:14:18 +0530

    v1.0.1

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import './mode-smali';
 import smaliIcon from './icon.svg';
 import plugin from '../plugin.json';
 
+const Url = acode.require('url');
 const { addMode, removeMode } = acode.require('aceModes');
 
 class AcodePlugin {
   #style;
-  #mode;
-  async init() {
+
+  async init(firstTime) {
     this.#style = <style
       textContent={
         `.file_type_smali::before{
@@ -26,19 +27,35 @@ class AcodePlugin {
 
     addMode('smali', 'smali', 'Smali');
     document.head.append(this.#style);
+
+    if (!firstTime) return;
+
+    editorManager.files.forEach(file => {
+      // update session mode
+      if (Url.extname(file.name) === '.smali') {
+        file.session.setMode('ace/mode/smali');
+      }
+    });
   }
 
   async destroy() {
     this.#style.remove();
     removeMode('smali');
+
+    editorManager.files.forEach(file => {
+      // update session mode
+      if (Url.extname(file.name) === '.smali') {
+        file.session.setMode('ace/mode/text');
+      }
+    });
   }
 
 }
 
 if (window.acode) {
   const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, () => {
-    acodePlugin.init();
+  acode.setPluginInit(plugin.id, (baseUrl, $page, options) => {
+    acodePlugin.init(options.firstInit);
   });
   acode.setPluginUnmount(plugin.id, () => {
     acodePlugin.destroy();
